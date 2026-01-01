22d0941bc50f1f163ed7f351303f34b0119a384c
Mohamed Abdulhamid <103602282+Mlknzii@users.noreply.github.com>
2026-01-01 16:40:44 +0200

    Change image remote patterns for HTTPS support
    Updated image remote patterns to use HTTPS and a new hostname.

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
-        pathname: "/static/uploads/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
+        protocol: "https",
+        hostname: "currency-detection-backend.onrender.com",
         pathname: "/static/uploads/**",
       },
     ],
