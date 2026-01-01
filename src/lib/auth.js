0b1c2dfdba646f5fb61d1fbafa9765238c704196
Mohamed Abdulhamid <103602282+Mlknzii@users.noreply.github.com>
2026-01-01 23:07:34 +0200

    Update setToken to include path option

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 const TOKEN_KEY = "token";
 
 export const setToken = (token) => {
-  Cookies.set(TOKEN_KEY, token, { expires: 7,secure: true, sameSite: "lax", });
+  Cookies.set(TOKEN_KEY, token, { expires: 7, sameSite: "lax", path: "/" });
 };
 
 export const getToken = () => Cookies.get(TOKEN_KEY);
