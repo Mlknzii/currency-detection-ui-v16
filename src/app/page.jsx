ed8456276c532fe0843359e53821a551b9305453
Mohamed Abdulhamid <103602282+Mlknzii@users.noreply.github.com>
2026-01-04 12:03:05 +0200

    Replace Link component with button for navigation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,18 @@
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { FaArrowLeft } from "react-icons/fa6";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <main className="">
       <div className="container mx-auto p-4 max-w-5xl grid place-items-center">
         <h1 className="text-4xl md:text-5xl font-bold text-center text-indigo-500">
           مرحبا بك في متعرف العملات... <br /> كيف يمكنني مساعدتك ؟
         </h1>
-        <Link href="/predict" className="btn btn-primary inline-block mt-6">
+        <button onClick={() => router.push("/predict")}  className="btn btn-primary inline-block mt-6">
           ابدأ الآن
           <FaArrowLeft className=" inline-block mr-2" />
-        </Link>
+        </button>
         {/* <CurrencyRecognizer /> */}
       </div>
     </main>
