692be5a4e34a724bbbf903becbdd4964ac79f990
Mohamed Abdulhamid <103602282+Mlknzii@users.noreply.github.com>
2026-01-04 11:35:44 +0200

    Remove default API_URL fallback in axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { getToken } from "./auth";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const apiClient = axios.create({
   baseURL: API_URL,
